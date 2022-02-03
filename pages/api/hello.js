6ca1f261841b5a1f32a06bde3edcabdaed0829e6
Pedro Pupo Sa da Costa <pedro@hublo.com>
2022-02-03 14:36:46 +0100

    add logging

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   if(req) {
-    console.log("payload": req.body);
+    console.log("payload", req.body);
   }
   res.status(200).json({ name: 'John Doe' })
 }
